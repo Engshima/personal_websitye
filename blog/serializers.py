63f206b76e2581ba175dca5e5e92b813dd46ba52
Engshima <sh.vatanpourasl.com>
2023-03-13 23:29:17 +0330

    commit msg

---
+++ blog/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import *
+
+class BlogDetailSerizlizer(serializers.ModelSerializer):
+    class Meta:
+       model = Article
+       fields = ['title','cover','content','created_time','category','author']
+
+    
+    
+    
+class CommentSerializer(serializers.ModelSerializer): 
+    
+    class  Meta:
+       model = Comment
+       fields = ['name','email','message']
+       
